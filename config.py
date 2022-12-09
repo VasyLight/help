58199a846e26b45a64eb77ca509ecb1fb50bea7b
VasyLight <51641318+VasyLight@users.noreply.github.com>
2022-12-09 16:04:22 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+USER_AGENT_PROXY_LIST = [
+    {
+        'http_proxy': 'http://94.242.55.10:80',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0'
+     },
+    {
+        'http_proxy': 'http://94.242.55.10:80',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0'
+     },
+    {
+        'http_proxy': 'http://94.242.55.10:80',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0'
+     },
+]
